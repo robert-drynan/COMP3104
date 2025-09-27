a7884e6d46339c6a2ab1e7a4cd3be097f24b7213
robert-drynan <165441895+robert-drynan@users.noreply.github.com>
2025-09-27 12:20:00 -0400

    Week1.2 - partial

---
+++ LabEx3W1/app/src/main/java/com/example/labex3w1/SampleData.kt
@@ -0,0 +1,15 @@
+package com.example.labex3w1
+
+object SampleData{
+    val items = listOf(
+        Message("Tony","hi"),
+        Message("Jon", "Hello"),
+        Message("Tony","hi"),
+        Message("Jon", "Hello"),
+        Message("Tony","hi"),
+        Message("Jon", "Hello"),
+        Message("Tony","hi"),
+        Message("Jon", "Hello")
+
+    )
+}
